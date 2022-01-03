a111b48c6d7d2f43fdefec4e300f11b30df40e68
webmasterdro <pdro.lima@pm.me>
2022-01-03 15:52:45 -0300

    Add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
 import { withTRPC } from '@trpc/next';
 import { AppType } from 'next/dist/shared/lib/utils';
 import type { AppRouter } from 'backend/routers'
